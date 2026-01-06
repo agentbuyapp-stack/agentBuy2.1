3d271f193f004b8f963f9125f10beed9facd0824
25LP9922 25LP9922 <25LP9922@admins-iMac.local>
2026-01-06 14:52:02 +0800

    agent dashboard start download shacn sidebar

---
+++ src/app/agent/_features/header.tsx
@@ -0,0 +1,3 @@
+export const Header = () => {
+  return <header className="w-full h-15 shrink-0 bg-green-500"></header>;
+};
--- src/app/agent/page.tsx
+++ src/app/agent/page.tsx
@@ -1,3 +1,11 @@
+import { SideBarSection } from "./_features/sidebarSection";
+import { Header } from "./_features/header";
+
 export default function Page() {
-  return <div>test admin page</div>;
+  return (
+    <div className="flex flex-col w-full h-screen">
+      <Header />
+      <SideBarSection />
+    </div>
+  );
 }
