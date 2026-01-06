c0722a12a2bce0614b35580d5845c3e4c454a6ac
25LP9922 25LP9922 <25LP9922@admins-iMac.local>
2026-01-06 19:44:20 +0800

    test

--- src/app/agent/page.tsx
+++ src/app/agent/page.tsx
@@ -1,11 +1,3 @@
-import { SideBarSection } from "./_features/sidebarSection";
-import { Header } from "./_features/header";
-
 export default function Page() {
-  return (
-    <div className="flex flex-col w-full h-screen">
-      <Header />
-      <SideBarSection />
-    </div>
-  );
+  return <></>;
 }
---
+++ src/app/agent/preOrder/_features/mainSection.tsx
@@ -0,0 +1,5 @@
+export const MainSection = () => {
+  return (
+    <div className="border border-zinc-200 rounded-lg w-312.5 h-200"></div>
+  );
+};
