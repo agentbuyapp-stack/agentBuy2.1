d9f1d65b7a0e5fd3f9d4118a565cadf71800a416
25LP9922 25LP9922 <25LP9922@admins-iMac.local>
2026-01-15 18:54:57 +0800

    check

--- src/app/agent/_features/header.tsx
+++ src/app/agent/_features/header.tsx
@@ -1,8 +1,11 @@
+"use client";
 import { UserButton } from "@clerk/nextjs";
 import { User2Icon } from "lucide-react";
 import { SidebarTrigger } from "@/components/ui/sidebar";
+import { useState } from "react";
 
 export const Header = () => {
+  const [state, setState] = useState([]);
   return (
     <header className="w-full h-16 shrink-0 flex items-center px-10 justify-between">
       <div className="flex items-center gap-2 px-2 py-1 shrink-0">
@@ -14,11 +17,11 @@ export const Header = () => {
           <span className="text-sm font-semibold">Agent Buy</span>
           <span className="text-xs text-muted-foreground">Agent Dashboard</span>
         </div>
-        <button>
-          <UserButton />
-          {/* <User2Icon color="black" /> */}
-        </button>
       </div>
+      <UserButton />
+      <button className="cursor-pointer">
+        <User2Icon />
+      </button>
     </header>
   );
 };
--- src/app/agent/preOrder/_features/mainSection.tsx
+++ src/app/agent/preOrder/_features/mainSection.tsx
@@ -3,20 +3,12 @@ import { Button } from "@/components/ui/button";
 import { Card } from "../_components/cards";
 import { useRouter } from "next/navigation";
 import { useUser } from "@clerk/nextjs";
-import { useEffect } from "react";
 
 export const MainSection = () => {
-  const { user, isLoaded } = useUser();
-  console.log(isLoaded, "is loaded");
-
-  useEffect(() => {
-    if (!user || !isLoaded) return;
-    if (isLoaded === true) {
-      console.log("unsafe:", user.unsafeMetadata);
-    }
-  }, [user, isLoaded]);
-
   const router = useRouter();
+  const { user } = useUser();
+  console.log("USER DATA:", user);
+
   return (
     <div className="w-full h-full p-6">
       <header className="flex flex-col">
@@ -37,13 +29,6 @@ export const MainSection = () => {
         </div>
       </header>
       <div className="flex flex-wrap justify-evenly gap-y-7">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
         <Card />
       </div>
     </div>
